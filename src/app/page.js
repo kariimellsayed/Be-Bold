e031134f9e5e0da2f635557cd9afc8767048d9a4
kariimellsayed <kariimellsayed@gmail.com>
2025-07-07 19:20:05 +0300

    some edits

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Hero from "./_sections/Hero";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full px-4 sm:px-10 py-10 flex flex-col gap-24 font-body">
+    <main className="min-h-screen w-full px-4 sm:px-10 py-10 font-body">
       <Hero />
     </main>
   );
